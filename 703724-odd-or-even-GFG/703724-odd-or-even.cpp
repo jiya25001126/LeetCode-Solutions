39b3607a13929cd15821ea348a3835fc1d407598
JIYA JAISWAL <jiya.25001126@kiet.edu>
2026-07-13 12:32:07 +0530

    Added solution - LeetHub

---
+++ 703724-odd-or-even-GFG/703724-odd-or-even.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    bool isEven(int n) {
+        if(n%2==0){
+            return true;
+        }
+        return false;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
