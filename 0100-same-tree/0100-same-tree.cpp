1ffb18e87ead2498c4f6029e334b69f2717d1e27
JIYA JAISWAL <jiya.25001126@kiet.edu>
2026-07-15 12:55:23 +0530

    Time: 0 ms (100%), Space: 12.8 MB (48.06%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0100-same-tree/0100-same-tree.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p==NULL || q==NULL){
+            return p==q;
+        }
+        bool isLeftSame = isSameTree(p->left, q->left);
+        bool isRightSame = isSameTree(p->right, q->right);
+        return isLeftSame && isRightSame && p->val == q->val;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
