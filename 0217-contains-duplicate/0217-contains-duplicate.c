42a14d8eba69e4e64df4236b901dee517a7fdc52
JIYA JAISWAL <jiya.25001126@kiet.edu>
2026-07-19 17:39:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub - Time: O(numsSize^2), Space: O(1)

---
+++ 0217-contains-duplicate/0217-contains-duplicate.c
@@ -0,0 +1,14 @@
+int containsDuplicate(int* nums, int numsSize) {
+    for (int i = 0; i < numsSize; i++) {
+        for (int j = i + 1; j < numsSize; j++) {
+            if (nums[i] == nums[j]) {
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
